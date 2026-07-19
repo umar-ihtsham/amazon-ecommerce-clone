d3d499ae8f1e3c456b46f79124660dad52655040
umar-ihtsham <m.umarihtisham15@gmail.com>
2026-07-19 23:09:21 +0500

    test for backend and loadproducts and use of done function to wait for products to load

--- test-jasmine/checkout/renderOrderSummaryTests.js
+++ test-jasmine/checkout/renderOrderSummaryTests.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../checkout/orderSummary.js";
 import { loadFromStorage,cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 
 describe('test suite: renderOrderSummary',()=>{
@@ -8,6 +9,12 @@ describe('test suite: renderOrderSummary',()=>{
     const productId1="e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
     const productId2="15b6fc6f-327a-4ec4-896f-486349e85a3d";
     const productId3="83d4ca15-0f35-48f5-b7a3-1ea210004f2e";
+
+    beforeAll((done)=>{
+        loadProducts(()=>{
+        done();
+        });
+    });
 
     beforeEach(()=>{
             document.querySelector(".Js-test-container").innerHTML=`
